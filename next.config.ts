ab7e1839d319806e8a377b8c40b88911b0898bf7
Hossein Shahverdi <h.shahverdi1997@gmail.com>
2026-09-25 17:45:11 +0330

    Static export for Cloudflare Pages

--- next.config.ts
+++ next.config.ts
@@ -1,9 +1,9 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
+  output: "export",
   reactStrictMode: true,
   poweredByHeader: false,
-  devIndicators: false,
 };
 
 export default nextConfig;
